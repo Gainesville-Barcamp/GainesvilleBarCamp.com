9fd27bc94f876c79df5d99107bde222183b4f317
jonchurch <me@jonchurch.com>
2022-04-12 18:59:54 -0400

    fix the deploy script conflicts

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,5 @@ git add .
 git commit -m "Automatic asset commit."
 git push origin gh-pages
 git checkout master
-<<<<<<< HEAD
-<<<<<<< HEAD
 git clean -fd
 yarn build
-=======
-npx yarn build
->>>>>>> master
-=======
-yarn build
->>>>>>> master
