e481aca89b5f9fb70b1e7707e6eb19cf9f3877fc
jonchurch <me@jonchurch.com>
2022-04-12 17:54:18 -0400

    Automatic asset commit.

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,9 @@
 git push origin master
 git checkout gh-pages
 git merge -v --no-edit master
-yarn build
+npx yarn build
 git add .
 git commit -m "Automatic asset commit."
 git push origin gh-pages
 git checkout master
-yarn build
+npx yarn build
