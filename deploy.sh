17374ee0fe0ed5f7284463f81890008f0fe91410
Mike Herchel <mike@herchel.com>
2022-04-16 13:40:51 -0400

    Remove shit out of deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-# https://bit.ly/pipefail
-set -euxo pipefail
-
 git push origin master
 git checkout gh-pages
 git merge -v --no-edit master
-yarn build
 git add .
 git commit -m "Automatic asset commit."
 git push origin gh-pages
 git checkout master
-yarn build
