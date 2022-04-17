93a8f105c9673865cdf8d66556cef3da50a5d1b3
Mike Herchel <mike@herchel.com>
2022-04-16 20:13:38 -0400

    Automatic asset commit.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Static HTML website for [Gainesvillebarcamp.com](http://www.gainesvillebarcamp.com)
 ## To install
 1. Clone the repository
-2. Install Gulp globally: `yarn global add parcel-bundler`
-2. Run `yarn install`
-4. Run `yarn start`
+2. Install Gulp globally: `npm install --global gulp-cli`
+3. Install Yarn: `npm install --global yarn`
+4. Run `yarn install`
+5. Run `yarn start`
 
 ## How to contribute
 1. Create a feature branch
 2. Push it up
-3. Create a pull request. [Hub](https://hub.github.com/) can make this process pretty easy.
+3. Create a pull request.
 
 
 ## How to commit deploy
---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+(() => {
+  const body = document.body;
+
+  function setTheme(theme) {
+    body.classList = ""
+    body.classList.add(theme);
+    var metaThemeColor = document.querySelector("meta[name=theme-color]");
+
+    switch (theme) {
+      case "cotton-candy":
+        metaThemeColor.setAttribute("content", '#00dbde');
+        break;
+      case "sunset":
+        metaThemeColor.setAttribute("content", '#FF1361');
+        break;
+      case "mist":
+        metaThemeColor.setAttribute("content", '#35eb93');
+        break;
+      default:
+        metaThemeColor.setAttribute("content", '#cc208e');
+    }
+    return false;
+  }
+
+  function handleClick(e) {
+    if (e.target.matches('a')) {
+      setTheme(e.target.dataset.theme);
+    }
+  }
+
+  document.querySelector('.themes').addEventListener('click', handleClick);
+})();
